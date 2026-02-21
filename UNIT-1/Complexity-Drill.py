525ad708a8a46478a65bfad5b89b96b92cc05dea
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2026-02-21 16:34:53 +0530

    Added the code for the single loop

--- UNIT-1/Complexity-Drill.py
+++ UNIT-1/Complexity-Drill.py
@@ -5,6 +5,20 @@
 # STRUCTURES AND CASE ANALYSIS .
 
 # 1. SINGLE LOOP - O(n)
+
+def single_loop(n):
+
+    count=0
+    for i in range(n):
+        count+=1
+
+    print("Number of operations performed :",count)
+    print("Time Complexity - O(n)")
+    print(f"Justification : Loop runs {n} times exactly .")
+    print("Linear growth with input size.")
+
+single_loop(int(input("Enter the number of iterations :")))
+
 # 2. NESTED LOOP - O(n^2)
 # 3. TRIANGULAR LOOP - O(n^2)
 # 4. HALVING LOOP - O(log(n))
