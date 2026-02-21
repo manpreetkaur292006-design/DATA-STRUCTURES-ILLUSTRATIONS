316e9117b548602f576f540e454a79f835308e0b
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2026-02-21 16:43:27 +0530

    Added the code for the triangular loop

--- UNIT-1/Complexity-Drill.py
+++ UNIT-1/Complexity-Drill.py
@@ -34,11 +34,27 @@ def nested_loop(n):
     print(f"Number of operations performed : {count}")
     print("Time Complexity - O(n^2)")
     print(f"Justification : Outer loop runs {n} times, inner loop runs {n} times each.")
-    print("Quadratic Growth - disastrous for large n")
+    print("Quadratic Growth - disastrous for large n.")
 
 nested_loop(int(input("Enter the number of iterations :")))
 
 
 # 3. TRIANGULAR LOOP - O(n^2)
+
+def triangular_loop(n):
+
+    count=0
+
+    for i in range(1,n+1):
+        for j in range(i):
+            count+=1
+
+    print(f"Number of operations performed : {count}")
+    print("Time Complexity - O(n^2)")
+    print(f"Justification : Inner loop is sum to ({n}**2)/2 , n^2/2 operations.")
+    print("Quardratic Growth - despite it is triangular pattern.")
+triangular_loop(int(input("Enter the number of iterations :")))
+
+
 # 4. HALVING LOOP - O(log(n))
 
