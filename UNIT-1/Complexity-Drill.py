540ba04df89940a7d62247c9137c3eab1e22aa18
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2026-02-21 16:39:03 +0530

    Added the code for the nested loop

--- UNIT-1/Complexity-Drill.py
+++ UNIT-1/Complexity-Drill.py
@@ -9,6 +9,7 @@
 def single_loop(n):
 
     count=0
+
     for i in range(n):
         count+=1
 
@@ -19,7 +20,25 @@ def single_loop(n):
 
 single_loop(int(input("Enter the number of iterations :")))
 
+
 # 2. NESTED LOOP - O(n^2)
+
+def nested_loop(n):
+
+    count=0
+
+    for i in range(n):
+        for j in range(n):
+            count+=1
+
+    print(f"Number of operations performed : {count}")
+    print("Time Complexity - O(n^2)")
+    print(f"Justification : Outer loop runs {n} times, inner loop runs {n} times each.")
+    print("Quadratic Growth - disastrous for large n")
+
+nested_loop(int(input("Enter the number of iterations :")))
+
+
 # 3. TRIANGULAR LOOP - O(n^2)
 # 4. HALVING LOOP - O(log(n))
 
