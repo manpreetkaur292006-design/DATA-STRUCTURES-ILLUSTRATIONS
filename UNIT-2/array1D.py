2e6f9b55b38b6fd79ccea06b450622d451e5b278
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2026-03-05 18:29:25 +0530

    added some comments

---
+++ UNIT-2/array1D.py
@@ -0,0 +1,8 @@
+
+# ARRAY 1D (OPERATIONS + SHIFTING COST)
+
+# UNDERSTANDING HOW TO INSERT/DELETE IN ARRAYS CAUSES SHIFTING AND IMPACTS COMPLEXITY.
+
+# 1-D ARRAY : A one-dimensional (1D) array is a linear data structure that
+#             stores a sequence of elements of the same data type in contiguous memory locations, 
+#             accessed using a single index (subscript).
