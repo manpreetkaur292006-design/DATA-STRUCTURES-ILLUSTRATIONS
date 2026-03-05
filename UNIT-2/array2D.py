010a2dd61ec6da5f61db9cd9c3109c7013da43d9
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2026-03-05 18:29:56 +0530

    Added some comments

---
+++ UNIT-2/array2D.py
@@ -0,0 +1,7 @@
+
+# ARRAY 2D (MATRIX TRAVERSAL + OPERATIONS)
+
+# PRACTISING 2D ARRAYS FOR REAL-WORLD TABULAR DATA AND UNDERSTAND TRAVERSAL COMPLEXITY.
+
+# 2-D ARRAY : A two-dimensional (2D) array is an array of arrays, organizing data into rows and columns like
+#             a table or matrix. Elements are accessed using two indices: one for the row and one for the column.
