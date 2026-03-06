f8ddb0ba4c350f5990c738470a56647c81174faa
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2026-03-06 10:42:04 +0530

    added the define of the dll

--- UNIT-2/doubleLL.py
+++ UNIT-2/doubleLL.py
@@ -2,3 +2,7 @@
 # DOUBLY LINKED LIST (EXTENDED OPERATIONS)
 
 # LEARNING BIDIRECTIONAL LINKS AND CORRECT POINTER UPDATES
+
+# DOUBLY LINKED LIST : A doubly linked list is a type of linked list where each 
+#                      node contains a reference to both the next and the previous node. This 
+#                      allows for efficient traversal in both directions.
