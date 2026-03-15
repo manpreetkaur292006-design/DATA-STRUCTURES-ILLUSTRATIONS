c2a86d9dbee36cb89631c89c9b056d34b5b34a6a
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2026-03-15 15:14:45 +0530

    added the problem statement with basic define

---
+++ UNIT-2/singleLL.py
@@ -0,0 +1,9 @@
+
+# SINGLY LINKED LIST (CORE OPERATIONS)
+
+# IMPLEMENTING DYNAMIC INSERTION AND DELETION WITHOUT SHIFTING
+
+# SINGLY LINKED LIST : Linear data structure of nodes where each node contains data 
+#                      + next pointer to the next node. One-way traversal from head to tail (last node's next = null).
+ 
+# ANSWER : 
