db313543e4c980b878cc98b78658721435d0ec0f
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2026-03-15 15:15:17 +0530

    added the problem statement along with simple define

---
+++ UNIT-2/stackSLL.py
@@ -0,0 +1,9 @@
+
+# STACK USING SLL + PARENTHESES CHECKER
+
+# BUILDING STACK USING LINKED LIST AND APPLY TO BRACKET VALIDATIONS.
+
+# STACK USING SLL : Stack implemented on top of SLL where top pointer = stack top. 
+#                   Push/pop at head (O(1)). LIFO order using SLL nodes.
+
+# ANSWER :
