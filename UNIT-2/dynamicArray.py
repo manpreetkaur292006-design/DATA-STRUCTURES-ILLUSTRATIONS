79d93177f2f9a0928c6ae6a6c1d07f3b23c85a8b
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2026-03-21 10:47:13 +0530

    added comments for easy understanding of the code

--- UNIT-2/dynamicArray.py
+++ UNIT-2/dynamicArray.py
@@ -8,42 +8,45 @@
 
 # ANSWER :
 
-class DynamicArray:
+class DynamicArray:  # creating a class - (blueprint)
     def __init__(self):
-        self.size = 0        # Number of elements
-        self.capacity = 4    # Initial capacity
-        self.array = [None] * self.capacity
+        self.size = 0        # Number of elements - setting 0
+        self.capacity = 4    # Initial capacity - fixing the length
+        self.array = [None] * self.capacity  # creating four length empty array
     
     def print_state(self):
+        # this only shows the first size elements and ignores unused chairs
         print(f"Size: {self.size}, Capacity: {self.capacity}, Array: {' '.join(map(str, self.array[:self.size]))}")
     
     def append(self, value):
-        if self.size == self.capacity:
-            # RESIZE: Double capacity
+        if self.size == self.capacity:  # when the array is full 
+            # RESIZE: Double the capacity of the array
             old = self.array
-            self.capacity *= 2
-            self.array = [None] * self.capacity
-            for i in range(self.size):
+            self.capacity *= 2 
+            self.array = [None] * self.capacity  # creating a bigger array with none
+            for i in range(self.size):  # moving all the elements to the new array 
                 self.array[i] = old[i]
-            print(f"RESIZED: {self.capacity//2} → {self.capacity}")
+            print(f"RESIZED: {self.capacity//2} → {self.capacity}")  # printing the message
         
-        self.array[self.size] = value
-        self.size += 1
+        self.array[self.size] = value # always entering the values to the next empty place available 
+        self.size += 1  # increasing the size count by 1
     
     def pop(self):
-        if self.size == 0:
+
+        # checking whether the array is empty if empty printing messages and returning none
+        if self.size == 0:  
             print("Empty array")
             return None
         
-        self.size -= 1
-        popped = self.array[self.size]
+        self.size -= 1 # decreasing the size count by 1
+        popped = self.array[self.size]  # saving which element left
         self.array[self.size] = None  # Optional: clear slot
-        print(f"Popped: {popped}")
+        print(f"Popped: {popped}")  # printing message and returning popped
         return popped
 
-# Lab Demo
+# Lab Demo - testing the code by running all the functions created
 print("Dynamic Array Demo")
-da = DynamicArray()
+da = DynamicArray() # object creation
 
 # Append sequence (triggers resize)
 print("\n--- APPEND OPERATIONS ---")
